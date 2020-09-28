8c30b50ab3992cc43eda3fe204bbfb4c7fba6a86
Raylson Andrews <andrewsraylson@gmail.com>
2020-09-28 17:25:57 -0300

    adding chat data

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,57 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import {
+    Container,
+    Messages,
+    InputWrapper,
+    Input,
+    InputIcon
+} from './styles';
+
+const ChannelData: React.FC = () => {
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+    useEffect(() => {
+        const div = messagesRef.current;
+
+        if (div) {
+            div.scrollTop = div.scrollHeight;
+        }
+    }, [messagesRef])
+
+  return (
+      <Container>
+          <Messages ref={messagesRef}>
+              {Array.from(Array(15).keys()).map((n) => (
+                  <ChannelMessage
+                    key={n}
+                    author="Raylson Andrews"
+                    date="22/07/2020"
+                    content="Hoje é meu aniversário."
+                  />
+              ))}
+
+              <ChannelMessage 
+                author="Diego Fernandes"
+                date="28/09/2019"
+                content={
+                    <>
+                        <Mention>@Raylson Andrews</Mention>, me carrega no CS de novo por favor?
+                    </>
+                }
+                hasMention
+                isBot
+              />
+          </Messages>
+
+          <InputWrapper>
+            <Input type="text" placeholder="Conversar em #chat-livre"/>
+            <InputIcon />
+          </InputWrapper>
+      </Container>
+  );
+}
+
+export default ChannelData;
